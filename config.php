5ae211d319eb23eaf6b18a8498835fe81f734edc
Malebestia <vair81@gmail.com>
2023-05-23 10:08:10 +0200

    .

--- config.php
+++ config.php
@@ -7,36 +7,23 @@ $moduleName = 'Payment';
 return [
     'baseUrl' => '',
     'production' => false,
-<<<<<<< HEAD
     'siteName' => 'Modulo '.$moduleName,
     'siteDescription' => 'Modulo '.$moduleName,
-=======
-    'siteName' => 'Modulo Media',
-    'siteDescription' => 'Modulo Media',
->>>>>>> b15ad18 (.)
     'lang' => 'it',
 
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-<<<<<<< HEAD
                 //return $page->lang.'/posts/'.Str::slug($page->getFilename());
                 //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
 
                 return 'posts/'.Str::slug($page->getFilename());
-=======
-                return $page->lang.'/posts/'.Str::slug($page->getFilename());
->>>>>>> b15ad18 (.)
             },
         ],
         'docs' => [
             'path' => function ($page) {
-<<<<<<< HEAD
                 //return $page->lang.'/docs/'.Str::slug($page->getFilename());
                 return 'docs/'.Str::slug($page->getFilename());
-=======
-                return $page->lang.'/docs/'.Str::slug($page->getFilename());
->>>>>>> b15ad18 (.)
             },
         ],
     ],
@@ -63,7 +50,6 @@ return [
         }
     },/*
     'url' => function ($page, $path) {
-<<<<<<< HEAD
         return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
     */
@@ -71,23 +57,11 @@ return [
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-         //return url('/'.$page->lang.'/'.trimPath($path));
+        //return url('/'.$page->lang.'/'.trimPath($path));
         return url('/'.trimPath($path));
     },
 
     'children' => function ($page, $docs) {
         return $docs->where('parent_id', $page->id);
-=======
-        if (Str::startsWith($path, 'http')) {
-            return $path;
-        }
-        // return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
-        return url('/'.$page->lang.'/'.trimPath($path));
-    },
-
-    'children' => function ($page, $docs) {
-        return $docs->where('parent_id', $page->id);
-    // return [];
->>>>>>> b15ad18 (.)
     },
 ];
